4888b807bb8423f66f4a257c9599502b56dbc915
munhoz2k <lucasmunhozarruda@gmail.com>
2024-03-19 23:05:36 -0300

    feat: shopping cart feature

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,40 @@
-import type { AppProps } from "next/app";
-import { globalStyles } from "@/styles/global";
-import logoImg from '@/assets/logo.svg'
 import Image from "next/image";
+import type { AppProps } from "next/app";
+
+import CartContextProvider from "@/contexts/cartContext";
+import Cart from "@/components/cart";
+import CartButton from "@/components/cartButton";
+
+import { globalStyles } from "@/styles/global";
 import { Container, Header, LogoContainer } from "@/styles/pages/app";
+
+import logoImg from '@/assets/logo.svg'
 
 globalStyles()
 export default function App({ Component, pageProps }: AppProps) {
+  function openCloseCart() {
+    document.getElementById('cart-container').classList.toggle('cart-open')
+  }
+
   return (
     <Container>
-      <Header>
-        <LogoContainer>
-          <Image src={logoImg} alt="" />
-          <div>
-            <h1>Igniters</h1>
-            <p>shop</p>
-          </div>
-        </LogoContainer>
-      </Header>
+      <CartContextProvider>
+        <Header>
+          <LogoContainer>
+            <Image src={logoImg} alt="" />
+            <div>
+              <h1>Igniters</h1>
+              <p>shop</p>
+            </div>
+          </LogoContainer>
 
-      <Component {...pageProps} />
+          <CartButton openCloseFunction={openCloseCart} />
+        </Header>
+
+        <Cart id='cart-container' openCloseFunction={openCloseCart} />
+
+        <Component {...pageProps} />
+      </CartContextProvider>
     </Container>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,33 @@
 import Stripe from "stripe";
 import Image from "next/image";
 import Link from "next/link"
+import Head from "next/head";
 import { GetStaticProps } from "next";
+import { MouseEventHandler, useContext } from "react";
 import { useKeenSlider } from "keen-slider/react";
-
+import { ShoppingBag } from "phosphor-react";
 
 import { stripe } from "@/lib/stripe";
+import { formatPrice } from "@/lib/formatPrice";
+import { CartContext } from "@/contexts/cartContext";
+
 import { HomeContainer, Product, ProductFooter } from "@/styles/pages/home";
 
 import "keen-slider/keen-slider.min.css"
-import getBase64 from "@/lib/getBase64";
-import Head from "next/head";
 
 interface HomeProps {
   products: {
     id: string
     name: string
     imageUrl: string
-    imageBlurData: string
-    formatedPrice: string
+    defaultPriceId: string
+    priceInCents: number
   }[]
 }
 
 export default function Home({ products }: HomeProps) {
+  const { addItem, cartItemsState } = useContext(CartContext)
+
   const [ sliderRef ] = useKeenSlider({
     slides: {
       perView: 2.75,
@@ -35,9 +40,9 @@ export default function Home({ products }: HomeProps) {
       <Head>
         <title>Home | Igniters</title>
       </Head>
+
       <HomeContainer ref={sliderRef} className="keen-slider">
-
-
+        <div className="left-shadow"></div>
         {products.map(p => {
           return (
             <Link href={`/products/${p.id}`} key={p.id} prefetch={false}>
@@ -45,20 +50,32 @@ export default function Home({ products }: HomeProps) {
                 <Image
                   src={p.imageUrl}
                   alt="Imagem da Camiseta"
-                  placeholder="blur"
-                  blurDataURL={p.imageBlurData}
                   width={520}
                   height={480}
                 />
 
                 <ProductFooter>
-                  <strong>{p.name}</strong>
-                  <span>{p.formatedPrice}</span>
+                  <div>
+                    <strong>{p.name}</strong>
+                    <span>{formatPrice(p.priceInCents)}</span>
+                  </div>
+
+                  <button 
+                    disabled={cartItemsState.length >= 10}
+                    onClick={(e) => {
+                      e.preventDefault();
+                      e.stopPropagation();
+                      addItem(p)
+                    }}
+                  >
+                    <ShoppingBag size={32} />
+                  </button>
                 </ProductFooter>
               </Product>
             </Link>
           )
         })}
+        <div className="right-shadow"></div>
       </HomeContainer>
     </>
   );
@@ -69,21 +86,15 @@ export const getStaticProps: GetStaticProps = async () => {
     expand: ['data.default_price']
   })
 
-  const base64Promises = data.map(p => getBase64(p.images[0]))
-  const base64Array = await Promise.all(base64Promises)
-
-  const products = data.map((product, i) => {
+  const products = data.map((product) => {
     const price = product.default_price as Stripe.Price
 
     return {
       id: product.id,
       name: product.name,
       imageUrl: product.images[0],
-      imageBlurData: base64Array[i],
-      formatedPrice: (price.unit_amount / 100).toLocaleString('pt-BR', {
-        style: 'currency',
-        currency: 'BRL'
-      })
+      defaultPriceId: price.id,
+      priceInCents: price.unit_amount
     }
   })
 
