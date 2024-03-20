2b99f27a685056d0738eeb1488b3a83c16c695fa
munhoz2k <lucasmunhozarruda@gmail.com>
2024-03-19 23:08:39 -0300

    chore: checkout multiple items

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -3,11 +3,11 @@ import { stripe } from "@/lib/stripe";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export type CheckoutBody = {
-  priceId: string;
+  priceIds: string[]
 };
 
 export type CheckoutRes = {
-  checkoutUrl?: string;
+  checkoutUrl?: string
   error?: string
 };
 
@@ -19,21 +19,25 @@ export default async function handler(
     return res.status(405).json({ error: 'Method not allowed.' })
   }
 
-  const { priceId } = req.body as CheckoutBody
+  const { priceIds } = req.body as CheckoutBody
 
-  if (!priceId) {
+  if (priceIds.length <= 0) {
     return res.status(400).json({ error: 'Product not found...' })
   }
 
+  const lineItems = priceIds.reduce((acc, cur) => {
+    const item = acc.find(item => item.valor === cur);
+
+    if (item) {
+      item.quantity++;
+    } else {
+      acc.push({ price: cur, quantity: 1 });
+    }
+    return acc;
+  }, []);
+
   const checkoutSession = await stripe.checkout.sessions.create({
-    line_items: [
-      {
-        price: priceId,
-        quantity: 1,
-        
-      }
-    ],
-    
+    line_items: lineItems,
     mode: 'payment',
     success_url: `${process.env.NEXT_APP_URL}/success?session_id={CHECKOUT_SESSION_ID}`,
     cancel_url: `${process.env.NEXT_APP_URL}/`,
